84266085dce2e203e31dc69f29b71da0c36cc3f4
wowilliam <wo.william@hotmail.com>
2022-07-19 18:50:40 -0300

    fundamentos angular

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PrimeiroComponenteComponent
   ],
   imports: [
     BrowserModule,
