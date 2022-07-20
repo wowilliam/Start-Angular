cddc44c847eb8fc0af9f81480e3f269598d9d321
wowilliam <wo.william@hotmail.com>
2022-07-20 20:26:42 -0300

    fundamentos angular

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -11,10 +11,11 @@ import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PrimeiroComponenteComponent, SegundoComponenteComponent, MultiplicaPorPipe, PipeCustomizadoComponent, TwoWayDataBindingComponent, RenderizandoListasComponent, ComponentePersonalizadoComponent
+    AppComponent, PrimeiroComponenteComponent, SegundoComponenteComponent, MultiplicaPorPipe, PipeCustomizadoComponent, TwoWayDataBindingComponent, RenderizandoListasComponent, ComponentePersonalizadoComponent, ComponentePaiComponent
   ],
   imports: [
     BrowserModule,
