84752b5d78a6e421ecd96a46c0b22943a9346fe1
wowilliam <wo.william@hotmail.com>
2022-07-22 17:45:27 -0300

    fundamentos angular

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
     SegundaPaginaComponent,
-    PaginaComParametrosComponent
+    PaginaComParametrosComponent,
+    PaginaProtegidaComponent
   ],
   imports: [
     BrowserModule,
