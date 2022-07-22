6005a59d4b006bf4de86ca9b1d5c6e58c4aedd6c
wowilliam <wo.william@hotmail.com>
2022-07-22 17:08:36 -0300

    fundamentos angular

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 const routes: Routes = [
-   {path: "primeira-pagina", component: PrimeiraPaginaComponent }
-   {path: "segunda-pagina", component: SegundaPaginaComponent }
+   {path: "primeira-pagina", component: PrimeiraPaginaComponent },
+   {path: "segunda-pagina", component: SegundaPaginaComponent },
+   {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
+   {path: "pagina-com-parametros/id", component: PaginaComParametrosComponent},
+   
 
 ]
 @NgModule({
