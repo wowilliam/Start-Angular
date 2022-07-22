0893c22ff534a9e5f2c1e401170a6bdcee855da8
wowilliam <wo.william@hotmail.com>
2022-07-22 15:42:52 -0300

    fundamentos angular

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+
 
 const routes: Routes = [
-   {path: "primeira-pagina"}
+   {path: "primeira-pagina", component: PrimeiraPaginaComponent }
 
 ]
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes) ]
+  imports: [RouterModule.forRoot(routes) ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
